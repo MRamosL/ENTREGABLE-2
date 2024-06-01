ffd49723ae526bce4f52a452936b7586796851ed
MRamosL <161747557+MRamosL@users.noreply.github.com>
2024-06-01 02:41:37 -0500

    Update Empleados.php
    cambio

--- PROYECTO_MVC/Empleados.php
+++ PROYECTO_MVC/Empleados.php
@@ -98,7 +98,9 @@
                         <button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#editarRegistroModal'><img src='icons/pencil-solid.svg'></button>
                     </td>
                     <td>
-                        <button type='button' class='btn btn-danger'  onclick='openModalProfesor({$row['id']})' data-bs-toggle='modal' data-bs-target='#modalEmpresa'><img src='icons/trash-solid.svg'></button>
+                    <button type='button' class='btn btn-info' onclick='openModalProfesor({$row['id']})' data-bs-toggle='modal' data-bs-target='#modalProfesor'>
+                    <img src='icons/trash-solid.svg'>
+                </button>
                     </td>
                 </tr>
                 ";
@@ -109,7 +111,7 @@
         ?>  
     </table>
 </body>
-
+  <!-- Insertar datos -->
 <?php
 include 'db/db.php'; // Incluye el archivo de conexión a la base de datos
 
@@ -124,107 +126,107 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['insertar'])) {
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $edad = $_POST['edad'];
-    $salario = $_POST['salario'];
+    $salario = $_POST['salario']; // Mantén el nombre del campo consistente
     $area = $_POST['area'];
 
     // Inserta en la base de datos
-    $sql = "INSERT INTO Empleados (id, nombre, apellido, edad, salario, area) VALUES (?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO Empleados (id, nombre, apellido, edad, salario_soles, area) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("isssds", $id, $nombre, $apellido, $edad, $salario, $area);
+    
+    if ($stmt) {
+        $stmt->bind_param("issids", $id, $nombre, $apellido, $edad, $salario, $area);
 
-    if ($stmt->execute()) {
-        // Registro exitoso, asigna mensaje de éxito
-        $insertar_success_message = "Datos insertados correctamente";
+        if ($stmt->execute()) {
+            // Registro exitoso, asigna mensaje de éxito
+            $insertar_success_message = "Datos insertados correctamente";
+        } else {
+            // Error en el registro, asigna mensaje de error
+            $insertar_error_message = "Error al insertar datos: " . $stmt->error;
+        }
+
+        // Cierra la conexión
+        $stmt->close();
     } else {
-        // Error en el registro, asigna mensaje de error
-        $insertar_error_message = "Error al insertar datos: " . $stmt->error;
+        // Error en la preparación de la consulta
+        $insertar_error_message = "Error al preparar la consulta: " . $conn->error;
     }
-
-    // Cierra la conexión
-    $stmt->close();
 }
 
 // Cierra la conexión a la base de datos
 $conn->close();
 ?>
 
-<!-- Modal de Inserción -->
-<div id="insertarModal" class="modal">
-    <div class="modal-content">
-        <span class="close" onclick="closeModal()">&times;</span>
-        <hr>
-        <h2>Insertar Datos</h2>
-        <hr> 
-        <form id="insertarForm" action="" method="POST">
-            <div class="form-group">
-                <label for="id">ID</label>
-                <input type="text" name="id" id="id" class="form-control" placeholder="ID" required>              
-            </div><br>
-            <div class="form-group">
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre" required>              
-            </div><br>
-            <div class="form-group">
-                <label for="apellido">Apellido</label>
-                <input type="text" name="apellido" id="apellido" class="form-control" placeholder="Apellido" required>              
-            </div><br>
-            <div class="form-group">
-                <label for="edad">Edad</label>
-                <input type="number" name="edad" id="edad" class="form-control" placeholder="Edad" required>              
-            </div><br>
-            <div class="form-group">
-                <label for="salario">Salario</label>
-                <input type="number" step="0.01" name="salario" id="salario" class="form-control" placeholder="Salario" required>              
-            </div><br>
-            <div class="form-group">
-                <label for="area">Área</label>
-                <input type="text" name="area" id="area" class="form-control" placeholder="Área" required>              
-            </div><br>
-            <?php if (isset($_POST['insertar'])): ?>
-                <?php if ($insertar_error_message): ?>
-                    <p id="error-message" class="error-message"><?php echo $insertar_error_message; ?></p>
-                <?php endif; ?>
-                <?php if ($insertar_success_message): ?>
-                    <p id="insertar-success-message" class="success-message"><?php echo $insertar_success_message; ?></p>
-                <?php endif; ?>
-            <?php endif; ?>
-            <button type="submit" name="insertar" class="btn btn-primary btn-block">Insertar</button>
-        </form>
+   <!-- Modal de Inserción -->
+   <div class="modal fade" id="insertarModal" tabindex="-1" aria-labelledby="insertarModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="insertarModalLabel">Insertar nuevos registros</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="insertarForm" action="" method="POST">
+                        <div class="mb-3">
+                            <label for="id" class="form-label">ID</label>
+                            <input type="text" name="id" id="id" class="form-control" placeholder="ID" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="nombre" class="form-label">Nombre</label>
+                            <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="apellido" class="form-label">Apellido</label>
+                            <input type="text" name="apellido" id="apellido" class="form-control" placeholder="Apellido" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="edad" class="form-label">Edad</label>
+                            <input type="number" name="edad" id="edad" class="form-control" placeholder="Edad" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="salario" class="form-label">Salario</label>
+                            <input type="number" step="0.01" name="salario" id="salario" class="form-control" placeholder="Salario" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="area" class="form-label">Área</label>
+                            <input type="text" name="area" id="area" class="form-control" placeholder="Área" required>
+                        </div>
+                        
+                        <button type="submit" name="insertar" class="btn btn-primary w-100">Insertar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-
-
-
+  <!-- Insertar datos fin  -->
 
 
     
-  
-
-    <?php
+  <?php
     include 'db/db.php';
 
-    // Verificar si se está enviando una solicitud POST para eliminarx
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_id_empleados'])) {
-        $id = $_POST['delete_id_empleados'];
+    // Verificar si se está enviando una solicitud POST para eliminar
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_id_profesor'])) {
+        $id = $_POST['delete_id_profesor'];
         $sql = "DELETE FROM Empleados WHERE id = $id";
         if (mysqli_query($db, $sql)) {
-            echo "<script>alert('Empleado eliminado exitosamente.'); window.location.href='';</script>";
+            echo "<script>alert('Profesor eliminado exitosamente.'); window.location.href='';</script>";
         } else {
             $error = mysqli_error($db);
-            echo "<script>alert('Error al eliminar el Empleado: $error');</script>";
+            echo "<script>alert('Error al eliminar el profesor: $error');</script>";
         }
     }
 
-    // Obtener todos los Empleados
+    // Obtener todos los profesores
     $sql = "SELECT * FROM Empleados";
     $result = mysqli_query($db, $sql);
-?>
-<!-- Modal para Eliminar Empleados -->
-<div class="modal fade" id="modalEmpresa" tabindex="-1" aria-labelledby="modalEmpresaLabel" aria-hidden="true">
+?> 
+
+<!-- Modal para Profesores -->
+<div class="modal fade" id="modalProfesor" tabindex="-1" aria-labelledby="modalProfesorLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalProfesorLabel">Estas seguro que deseas eliminar los datos del empleado</h5>
+                <h5 class="modal-title" id="modalProfesorLabel">Estas seguro que deseas eliminar los datos del profesor</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-footer">
@@ -245,57 +247,107 @@ $conn->close();
 </script>
 
 
-  <!--Editar-->
-  <div class="modal fade" id="editarRegistroModal" tabindex="-1" role="dialog" aria-labelledby="editarDatosModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editarDatosModalLabel">Insertar nuevos registros</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                        <!--<span aria-hidden="true">&times;</span>-->
-                    </button>
-                </div>
-                <form action="index.php?page=insertar" method="POST" name="registroForm" id="registroForm" class="text-left">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="id">ID</label>
-                            <input type="text" id="id" name="id" class="form-control" aria-describedby="idHelp">
-                            <small id="idHelp" class="form-text text-muted">Ingrese el id del profesor.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="nombre">Nombre</label>
-                            <input type="text" id="nombre" name="nombre" class="form-control" aria-describedby="nombreHelp">
-                            <small id="nombreHelp" class="form-text text-muted">Ingrese el nombre del empleado.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="apellido">Apellido</label>
-                            <input type="text" id="apellido" name="apellido" class="form-control" aria-describedby="apellidoHelp">
-                            <small id="apellidoHelp" class="form-text text-muted">Ingrese el apellido del empleado.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="edad">Edad</label>
-                            <input type="text" id="edad" name="edad" class="form-control" aria-describedby="edadHelp">
-                            <small id="edadHelp" class="form-text text-muted">Ingrese la edad del empleado.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="salario">Salario</label>
-                            <input type="text" id="salario" name="salario_soles" class="form-control" aria-describedby="salarioHelp">
-                            <small id="salarioHelp" class="form-text text-muted">Ingrese el salario del empleado.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="area">Área</label>
-                            <input type="text" id="area" name="area" class="form-control" aria-describedby="areaHelp">
-                            <small id="areaHelp" class="form-text text-muted">Ingrese el área del empleado.</small>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                        <button type="button" id="btnEditar" name="btnEditar" class="btn btn-primary">Editar</button>
-                    </div>
-                </form>
+<!-- Botón de Edición -->
+
+
+<!-- Modal de Edición -->
+<div class="modal fade" id="editarRegistroModal" tabindex="-1" role="dialog" aria-labelledby="editarDatosModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editarDatosModalLabel">Editar datos del empleado</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <form action="" method="POST" name="editarEmpleadoForm" id="editarEmpleadoForm" class="text-left">
+                <div class="modal-body">
+                    <input type="hidden" id="edit_id" name="edit_id">
+                    <div class="form-group">
+                        <label for="edit_nombre">Nombre</label>
+                        <input type="text" id="edit_nombre" name="edit_nombre" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit_apellido">Apellido</label>
+                        <input type="text" id="edit_apellido" name="edit_apellido" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit_edad">Edad</label>
+                        <input type="number" id="edit_edad" name="edit_edad" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit_salario">Salario</label>
+                        <input type="number" step="0.01" id="edit_salario" name="edit_salario" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit_area">Área</label>
+                        <input type="text" id="edit_area" name="edit_area" class="form-control" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="submit" id="btnEditar" name="btnEditar" class="btn btn-primary">Editar</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
+
+<!-- JavaScript para Abrir el Modal y Establecer el ID -->
+<script>
+    function openEditarModal(id, nombre, apellido, edad, salario, area) {
+        document.getElementById('edit_id').value = id;
+        document.getElementById('edit_nombre').value = nombre;
+        document.getElementById('edit_apellido').value = apellido;
+        document.getElementById('edit_edad').value = edad;
+        document.getElementById('edit_salario').value = salario;
+        document.getElementById('edit_area').value = area;
+        var myModal = new bootstrap.Modal(document.getElementById('editarRegistroModal'));
+        myModal.show();
+    }
+</script>
+<?php
+include 'db/db.php'; // Incluye el archivo de conexión a la base de datos
+
+// Verifica si se envió el formulario de edición
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['btnEditar'])) {
+    // Obtiene los datos del formulario
+    $id = $_POST['edit_id'];
+    $nombre = $_POST['edit_nombre'];
+    $apellido = $_POST['edit_apellido'];
+    $edad = $_POST['edit_edad'];
+    $salario = $_POST['edit_salario'];
+    $area = $_POST['edit_area'];
+
+    // Verifica si la conexión está establecida
+    if ($conn) {
+        // Prepara la consulta de actualización
+        $sql = "UPDATE Empleados SET nombre=?, apellido=?, edad=?, salario_soles=?, area=? WHERE id=?";
+        $stmt = $conn->prepare($sql);
+
+        if ($stmt) {
+            // Vincula los parámetros y ejecuta la consulta
+            $stmt->bind_param("ssidsi", $nombre, $apellido, $edad, $salario, $area, $id);
+
+            if ($stmt->execute()) {
+                echo "<script>alert('Datos actualizados correctamente');</script>";
+            } else {
+                echo "<script>alert('Error al actualizar datos: " . $stmt->error . "');</script>";
+            }
+
+            $stmt->close(); // Cierra la consulta preparada
+        } else {
+            echo "<script>alert('Error al preparar la consulta: " . $conn->error . "');</script>";
+        }
+    } else {
+        echo "<script>alert('Error: No se pudo conectar a la base de datos.');</script>";
+    }
+}
+?>
+
+
+
+
+
+
 <script>
     document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("formularioInsertar").addEventListener("submit", function(event) {
